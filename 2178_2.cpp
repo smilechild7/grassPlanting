59052d337484bef699db8e7bb8003d0a96abf23d
KIM SEONG EUN <smilechild1005@naver.com>
2023-08-22 18:27:12 +0900

    2178 예제는 다 맞는데...

--- 2178_2.cpp
+++ 2178_2.cpp
@@ -11,20 +11,50 @@ int main() {
     cin.tie(NULL); cout.tie(NULL);
 
     int N, M; cin >> N >> M;
-    int size = N*M;
-    int map[10000];
     queue<int> q;
+    int map[10000];
     bool visited[10000];
     vector <int> graph[10000];
 
-    for (int i=0; i<size; i++) {
-        char c; cin >> c;
-        map[i]=int(c);
+    for(int i=0; i<N*M; i++){
+        scanf("%1d", &map[i]);
+	}
+
+    for (int i=0; i<N; i++) {
+        for (int j=0; j<M-1; j++) {
+            if (map[j+M*i]==1 && map[j+M*i+1]==1) {
+                graph[j+M*i].push_back(j+M*i+1);
+                graph[j+M*i+1].push_back(j+M*i);
+            }
+        }
     }
 
-    
+    for (int i=0; i<N-1; i++) {
+        for (int j=0; j<M; j++) {
+            if (map[j+M*i]==1 && map[j+M*(i+1)]==1) {
+                graph[j+M*i].push_back(j+M*(i+1));
+                graph[j+M*(i+1)].push_back(j+M*i);
+            }
+        }
+    }
 
+    int t=0;
+    q.push(0);
+    while (!q.empty()) {
+        int k = q.front();
+        q.pop();
+        int s = graph[k].size();
 
+        for (int i=0; i<s; i++) {
+            int y = graph[k][i];
+            if (!visited[y]) {
+                q.push(y);
+                visited[y]=true;
+                map[y] = map[k]+1;
+            }
+        }
+    }
+    cout << map[N*M-1];
 
     return 0;
 }
