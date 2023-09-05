eaf269adf2cd6ea80a4c79870386e65921402008
KIM SEONG EUN <127196534+smilechild7@users.noreply.github.com>
2023-09-05 12:33:46 +0900

    에오스과제

---
+++ main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Home'),
+            leading: Icon(Icons.dehaze),
+            actions: [Icon(Icons.settings_outlined)],
+          ),
+          body: SafeArea(
+              child: Center(
+                  child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('assets/eos_logo.png', width: 300, height: 300)
+            ],
+          )))),
+    );
+  }
+}
