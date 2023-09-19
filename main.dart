47907acadc84015712d86f3eb209dfb82c903a01
KIM SEONG EUN <127196534+smilechild7@users.noreply.github.com>
2023-09-19 15:41:17 +0900

    eos homework 0919

--- main.dart
+++ main.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,82 +6,80 @@ void main() {
 }
 
 class MyApp extends StatefulWidget {
-  //
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  var _timer;
-  var _time = 0;
-  var _isRunning = false;
-  var _stopWatch;
+  var subjectList = ['국어', '수학'];
+  var timeList = [0, 0, 0];
+  var secList = [0, 0, 0];
+  var minList = [0, 0, 0];
+  var hourList = [0, 0, 0];
+  var isRunning = [false, false];
+
   @override
   void initState() {
-    super.initState(); // initState class 를 가져옴
-    _timer = Timer.periodic(Duration(seconds: 1),
-        _updateTimer); // 이것만 추가 ( 1초에 한번씩 _updateTimer를 실행)
+    super.initState();
+    for (var i = 0; i < 2; i++) {
+      _startTimer(i);
+    }
   }
 
-  void _updateTimer(Timer timer) {
-    setState(() {
-      if (_isRunning) {
-        _time++;
-      }
+  void _startTimer(int k) {
+    Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {
+        if (isRunning[k]) {
+          timeList[k]++;
+          secList[k] = timeList[k] % 60;
+          minList[k] = (timeList[k] ~/ 60) % 60;
+          hourList[k] = timeList[k] ~/ 3600;
+        }
+        timeList[2] = timeList[0] + timeList[1];
+        secList[2] = timeList[2] % 60;
+        minList[2] = (timeList[2] ~/ 60) % 60;
+        hourList[2] = timeList[2] ~/ 3600;
+      });
     });
   }
 
-  void _startTimer() {
+  void _pauseTimer(int k) {
     setState(() {
-      _isRunning = true;
-    });
-  }
-
-  void _pauseTimer() {
-    setState(() {
-      _isRunning = false;
+      isRunning[k] = false;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    var sec = (_time % 60).toString().padLeft(2, '0');
-    var min = (((_time / 60).toInt()) % 60.toInt())
-        .toString()
-        .padLeft(2, '0'); // (_time~/60)%60
-    var hour = ((_time / 3600).toInt()).toString().padLeft(2, '0');
-
-    var secZero = "";
-    var minZero = "";
-    var hourZero = "";
-    // if (sec < 10) secZero = "0";
-    // if (min < 10) minZero = "0";
-    // if (hour < 10) hourZero = "0";
+    var secZero = ["", "", ""];
+    var minZero = ["", "", ""];
+    var hourZero = ["", "", ""];
+    for (int j = 0; j < 3; j++) {
+      if (secList[j] < 10) secZero[j] = "0";
+      if (minList[j] < 10) minZero[j] = "0";
+      if (hourList[j] < 10) hourZero[j] = "0";
+    }
 
     return MaterialApp(
       home: Scaffold(
-          // 알아서 상중하로 나누어주는 위젯
-          appBar: AppBar(
-            backgroundColor: Colors.green,
-            title: const Text('EOS BASIC'),
-            centerTitle: true,
-            leading: Icon(Icons.dehaze), // 햄버거 아이콘
-            actions: [Icon(Icons.settings_outlined)], // 톱니바퀴(설정) 아이콘
-          ),
-          floatingActionButton: FloatingActionButton(
-            // 스크롤 해도 항상 떠 있는 위젯
-            onPressed: () {},
-            backgroundColor: Colors.green,
-            child: Icon(Icons.add), // + 표시
-          ),
-          body: SafeArea(
-              // 휴대폰 조건에 따라 화면이 잘리지 않도록 방지
-              child: Padding(
+        appBar: AppBar(
+          backgroundColor: Colors.green,
+          title: const Text('EOS BASIC'),
+          centerTitle: true,
+          leading: Icon(Icons.dehaze),
+          actions: [Icon(Icons.settings_outlined)],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: Colors.green,
+          child: Icon(Icons.add),
+        ),
+        body: SafeArea(
+          child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
             child: Column(
-              // 세로정렬
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Image.asset('assets/eos_logo.png', width: 200, height: 200),
@@ -90,7 +87,7 @@ class _MyAppState extends State<MyApp> {
                   height: 50,
                 ),
                 Text(
-                  "00:00:00",
+                  "${hourZero[2]}${hourList[2]} : ${minZero[2]}${minList[2]} : ${secZero[2]}${secList[2]}",
                   style: TextStyle(
                     fontSize: 35,
                     fontWeight: FontWeight.bold,
@@ -111,8 +108,14 @@ class _MyAppState extends State<MyApp> {
                     children: [
                       IconButton(
                         padding: EdgeInsets.all(0),
-                        onPressed: (_isRunning ? _pauseTimer : _startTimer),
-                        icon: _isRunning
+                        onPressed: () {
+                          if (isRunning[0]) {
+                            _pauseTimer(0);
+                          } else {
+                            isRunning[0] = true;
+                          }
+                        },
+                        icon: isRunning[0]
                             ? Icon(
                                 Icons.pause_circle,
                                 size: 40,
@@ -129,7 +132,7 @@ class _MyAppState extends State<MyApp> {
                         style: TextStyle(fontSize: 25),
                       ),
                       Text(
-                        "$hour : $min : $sec",
+                        "${hourZero[0]}${hourList[0]} : ${minZero[0]}${minList[0]} : ${secZero[0]}${secList[0]}",
                         style: TextStyle(fontSize: 25),
                       ),
                     ],
@@ -145,17 +148,33 @@ class _MyAppState extends State<MyApp> {
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Icon(
-                        Icons.play_circle,
-                        size: 40,
-                        color: Colors.green,
+                      IconButton(
+                        padding: EdgeInsets.all(0),
+                        onPressed: () {
+                          if (isRunning[1]) {
+                            _pauseTimer(1);
+                          } else {
+                            isRunning[1] = true;
+                          }
+                        },
+                        icon: isRunning[1]
+                            ? Icon(
+                                Icons.pause_circle,
+                                size: 40,
+                                color: Colors.green,
+                              )
+                            : Icon(
+                                Icons.play_circle,
+                                size: 40,
+                                color: Colors.green,
+                              ),
                       ),
                       Text(
                         "수학",
                         style: TextStyle(fontSize: 25),
                       ),
                       Text(
-                        "00:00:00",
+                        "${hourZero[1]}${hourList[1]} : ${minZero[1]}${minList[1]} : ${secZero[1]}${secList[1]}",
                         style: TextStyle(fontSize: 25),
                       ),
                     ],
@@ -167,7 +186,9 @@ class _MyAppState extends State<MyApp> {
                 ),
               ],
             ),
-          ))),
+          ),
+        ),
+      ),
     );
   }
 }
